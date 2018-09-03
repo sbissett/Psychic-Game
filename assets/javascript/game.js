fc2a6703ada1505f36664a2484b0b9bb9fbd5fe8
Stephen Bissett <steve.bissett5@gmail.com>
2018-09-03 15:12:54 -0600

    update

---
+++ assets/javascript/game.js
@@ -0,0 +1,27 @@
+// <script type="text/javascript">
+    
+
+
+var computerChoices = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","r","s","t","u","v","w","x","y","z"];
+
+// var playerChoice - prompt(Your guess so far)
+//Convert playerChoiceLower = playerChoice.toLowerCase();
+
+
+
+//* Player 1 chooses a random letter...and has 9 instances to either win or loose.
+
+
+//* If player 1 chooses a letter that matches the computer, player 1 wins a "set"
+
+//* When player 1 wins a set he advances +1 on the win column
+
+//* If play 1 attempts 9 instances and does NOT match the computer, player 1 looses and advances +1 on the loss column8
+
+//* Alert each incorrect result / do not alert correct result and player 1 wins
+
+//* When the player gets to either 8 wins or 8 looses the games is over and is reset to zero
+
+   
+
+</script>    
