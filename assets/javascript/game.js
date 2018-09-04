3eded41c9da7022aa40e05e0296b09a04f26333e
Stephen Bissett <steve.bissett5@gmail.com>
2018-09-04 11:22:43 -0600

    update

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,27 +1,50 @@
-// <script type="text/javascript">
     
+function yourGuess()
+
+{
+
+//create an array that lists out all the options a-z
+
+var alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","r","s","t","u","v","w","x","y","z"];
+
+// creat variables to hold the number of wins, losses
+
+var wins = 0;
+var losses = 0;
+var userguess = 8;
+var gameOver = false;
+
+    var computerChoice = alphabet[Math.floor(Math.randon() * alphabet.length)];
+
+    console.log(computerChoice)  
+
+// This function is run whenver the user presses a key
+
+document.onkeyup = function (event) {
+
+    // Determines which key was pressed
+
+    var userguess = event.key;
+
+    if(userguess = computerChoice) {
+        wins++;
+
+    }else{
+
+    userguesses--; 
+
+    }
+
+    if (userguesses === 0) 
+    {
+        lossess++
+
+    }
+}
+    
+    document.getElementById('wins').innerHTML = "Wins: " + wins;
+    documnet.getElementById('losses').innerHTML = "losses: " + losses;
+    document.getElementByID('userguesses').innerHTML = "userGuesses left; " + guesses left;
+}
 
 
-var computerChoices = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","r","s","t","u","v","w","x","y","z"];
-
-// var playerChoice - prompt(Your guess so far)
-//Convert playerChoiceLower = playerChoice.toLowerCase();
-
-
-
-//* Player 1 chooses a random letter...and has 9 instances to either win or loose.
-
-
-//* If player 1 chooses a letter that matches the computer, player 1 wins a "set"
-
-//* When player 1 wins a set he advances +1 on the win column
-
-//* If play 1 attempts 9 instances and does NOT match the computer, player 1 looses and advances +1 on the loss column8
-
-//* Alert each incorrect result / do not alert correct result and player 1 wins
-
-//* When the player gets to either 8 wins or 8 looses the games is over and is reset to zero
-
-   
-
-</script>    
