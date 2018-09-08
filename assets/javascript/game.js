335feb7a16c4257c2b44de315bccdbd132cb320f
Stephen Bissett <steve.bissett5@gmail.com>
2018-09-08 09:30:54 -0600

    update

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,6 @@
 //create an array that lists out all the options a-z
 
-var alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","r","s","t","u","v","w","x","y","z"];
+var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
 // creat variables to hold the number of wins, losses
 
@@ -9,37 +9,54 @@ var losses = 0;
 var guess = 8;
 var gameOver = false;
 
-    var computerChoice = alphabet[Math.floor(Math.random() * alphabet.length)];
+var computerChoice = alphabet[Math.floor(Math.random() * alphabet.length)];
 
-    console.log(computerChoice)  
+console.log("Welcome Stephen: " + wins);
+console.log("you wanna win or lose: " + losses);
+console.log("you have 8 guesses: " + guess);
+console.log("hows the choices coming along: " + computerChoice)
+
 
 // This function is run whenver the user presses a key
 
+
+
+
 document.onkeyup = function (event) {
+
+
 
     // Determines which key was pressed
 
-    var userguess = event.key;
 
-    if(userguess = computerChoice) {
+    var userguess = event.key;
+    console.log(userguess);
+
+    if (userguess === computerChoice) {
+        console.log("win");
+
         wins++;
 
-    }else{
+        document.getElementById('wins').innerHTML = wins;
 
-        userguess--; 
+    } else {
+        console.log("Fail", guess) 
+        
+        guess--;
+
+         }
+
+    if (guess === 0) {
+        console.log('You loose')
+        losses++;
+
+
+        document.getElementById('losses').innerHTML = losses;
+
 
     }
 
-    if (userguess === 0) 
-    {
-        losses++
-
-    }
-
-    
-    document.getElementById('wins').innerHTML = "Wins: " + wins;
-    documnet.getElementById('losses').innerHTML = "losses: " + losses;
-    document.getElementByID('userguess').innerHTML = "userguess left: " + guess;
+    document.getElementById('userguess').innerHTML = guess;
 
 }
 
